3f903a4497d587680a18be92e676f24f1903a32a
Guest Account with permissions <GuestAccount@Victorias-MacBook-Pro.local>
2020-02-19 16:44:54 +0200

    Initial photo gallery

--- MemeGenerator/Controllers/PhotoGallery/PhotoGalleryViewController.swift
+++ MemeGenerator/Controllers/PhotoGallery/PhotoGalleryViewController.swift
@@ -8,23 +8,42 @@
 
 import UIKit
 
-class PhotoGalleryViewController: UIViewController {
+let reuseIdentifier = "PhotoCell"
 
+class PhotoGalleryViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+            
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupBackground()
+    }
+
+    private func setupBackground() {
+        let gradient = CAGradientLayer()
+        gradient.frame.size = view.frame.size
+        gradient.startPoint = CGPoint(x: 0, y:0)
+        gradient.endPoint = CGPoint(x: 1, y: 1)
+        gradient.colors = [ #colorLiteral(red: 0.5058823529, green: 0.8784313725, blue: 0.5058823529, alpha: 1).cgColor , #colorLiteral(red: 0.1960784314, green: 0.8039215686, blue: 0.1960784314, alpha: 1).cgColor, #colorLiteral(red: 0.3215686275, green: 0.8352941176, blue: 0.3215686275, alpha: 1).cgColor]
+        view.layer.insertSublayer(gradient, at: 0)
+        collectionView.backgroundColor = #colorLiteral(red: 0.2235294118, green: 0.8549019608, blue: 0.2235294118, alpha: 1)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 50
     }
-    */
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell: UICollectionViewCell = collectionView.dequeueReusableCell( withReuseIdentifier: reuseIdentifier, for: indexPath) as UICollectionViewCell
+        
+        cell.backgroundColor = UIColor.red
+        
+        return cell
+    }
 
 }
--- MemeGenerator/Controllers/PhotoGallery/PhotoViewController.swift
+++ MemeGenerator/Controllers/PhotoGallery/PhotoViewController.swift
@@ -10,21 +10,20 @@ import UIKit
 
 class PhotoViewController: UIViewController {
 
+    @IBAction func exportButton(_ sender: Any) {
+        print("export")
+    }
+    
+    @IBAction func cancelButton(_ sender: Any) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
